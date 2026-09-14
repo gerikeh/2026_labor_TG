70d55a1fccf92dbe97f37eb3b843bf51bb53beac
gerikeh <geriboy0217@gmail.com>
2026-09-14 12:49:42 +0200

    Add files via upload

---
+++ WelcomeScreen/masodik.py
@@ -0,0 +1,17 @@
+
+# Ez a masodik labor feladatait tartalmazza
+felhasznalo_kora =int(15.76)
+felhasznalo_kora = input("Hány éves vagy?")
+felhasznalo_kora += 21
+felhasznalo_neve =  input('Kérem a nevet:')
+metszet = felhasznalo_neve[1:5]
+
+jegyek  = [2, 4,5 ,3]
+del jegyek[0]
+halmaz = {'magyar', 'angol', 'orosz', 3}
+hallgato = {"nev" : 'Jolan' , "kor" :18}
+print(hallgato)
+elem = halmaz
+print(halmaz )
+print("Szia", felhasznalo_neve ,"!" , jegyek[2])
+
